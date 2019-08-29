08206d784c80cdb4dded282aa9c3ed0362cb0a0d
Rodrigo Bisso <rodrigobissomachado@gmail.com>
2019-08-29 15:42:13 -0300

    last updates done at Thu 29 Aug 2019 03:42:13 PM -03

---
+++ git-push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+git pull
+find . -type f -name ._\* -exec rm -f {} \;
+git add .
+git ls-files --others --exclude-from=.git/info/exclude
+TIME=`date`
+git commit -m "last updates done at $TIME"
+git push
--- gpg-example.py
+++ gpg-example.py
@@ -1,6 +1,6 @@
 """
 NOMES: RODRIGO BISSO
-
+       GUSTAVO CARDOZO
 REQUISITOS:
     sudo apt install gpg
     pip3 install python-gnupg
@@ -19,68 +19,54 @@ import gnupg
 import os
 import sys
 
-if len(sys.argv) < 2 :
-    print("Uso: pyhton3 " +sys.argv[0]+ " <arquivo_mensagem>")
-    sys.exit(0) 
-
-file_message = sys.argv[1]
-
-gpg = gnupg.GPG(gnupghome=os.getcwd())
-imput_data = gpg.gen_key_input(
-        name_email='my@email.com',
-        passphrase='passphrase',
-)
-
-key = gpg.gen_key(imput_data)
-print(key)
-
-ascii_armored_public_keys = gpg.export_keys(key.fingerprint)
-ascii_armored_private_keys= gpg.export_keys(
-        keyids=key.fingerprint,
-        secret='True',
-        passphrase='passphrase'
-)
-
-# exporting keys
-with open('mykeyfile.asc', 'w') as f:
-    f.write(ascii_armored_public_keys)
-    f.write(ascii_armored_private_keys)
-
-with open('mykeyfile.asc', 'r') as f:
-    key_data = f.read()
-
-import_result = gpg.import_keys(key_data)
-for k in import_result.results:
-    print(k)
-
-#encrypting file
-
-with open(file_message,'rb') as f:
-    status=gpg.encrypt_file(
-        file=f,
-        recipients=['my@email.com'],
-        output='encrypted.txt.gpg',
-    )
-
-print(status.ok)
-print(status.status)
-print(status.stderr)
-print('~'*50)
-
-#decrypting file
-with open('encrypted.txt.gpg', 'rb') as f:
-    status = gpg.decrypt_file(
-        file=f,
-        passphrase='passphrase',
-        output='decrypted.txt',
+def export_keys(key): #local -> to file
+    
+    ascii_armored_public_keys = gpg.export_keys(key.fingerprint)
+    ascii_armored_private_keys= gpg.export_keys(
+            keyids=key.fingerprint,
+            secret='True',
+            passphrase='passphrase'
     )
     
+    with open('mykeyfile.asc', 'w') as f:
+        f.write(ascii_armored_public_keys)
+        f.write(ascii_armored_private_keys)
+
+def import_keys(): #local
+    with open('mykeyfile.asc', 'r') as f:
+        key_data = f.read()
+
+    import_result = gpg.import_keys(key_data)
+    for k in import_result.results:
+        print(k)
+
+    return import_result
+
 #Sending keys
 #gpg.send_keys(SERVER, PRIVATE_KEY_ID)
 
 #remote import keys
 #import_result = gpg.recv_keys('server-name', 'keyid1', 'keyid2', ...)
 
-print(status.ok)
-print(status.status)
-print(status.stderr)
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2 :
+        print("Uso: pyhton3 " +sys.argv[0]+ " <arquivo_mensagem>")
+        sys.exit(0) 
+
+    file_message = sys.argv[1]
+
+    gpg = gnupg.GPG(gnupghome=os.getcwd())
+    imput_data = gpg.gen_key_input(
+            name_email='my@email.com',
+            passphrase='passphrase',
+    )
+
+    key = gpg.gen_key(imput_data)
+    print(key)
+    
+    """
+    print(status.ok)
+    print(status.status)
+    print(status.stderr)
+    """

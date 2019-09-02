41d587edd7a3892ed642c2224b8c97edb9292cb9
Rodrigo Bisso <rodrigobissomachado@gmail.com>
2019-09-02 19:08:41 -0300

    last updates done

--- gpg-example.py
+++ gpg-example.py
@@ -9,11 +9,10 @@ diretorio precisa ter permissao 700 (chmod 700 <nome_dir>)
 criar um diretorio com essa permissão dentro do diretorio .gnupg (/home/<usr>/.gnupg/)
 
 TUTORIAL SEGUIDO:
-    https://gnupg.readthedocs.io/en/latest/
-    https://gist.github.com/ryantuck/56c5aaa8f9124422ac964629f4c8deb0
+    https://gnupg.readthedocs.io/en/latest/ *
+    https://pythonhosted.org/gnupg/gnupg.html *
     https://pymotw.com/2/argparse/
     http://zetcode.com/python/argparse/
-
 """
 
 import gnupg
@@ -21,69 +20,103 @@ import os
 import sys
 import argparse
 
-def export_keys(gpg, key): #local -> to file
-    
-    ascii_armored_public_keys = gpg.export_keys(key.fingerprint)
-    ascii_armored_private_keys= gpg.export_keys(
-            keyids=key.fingerprint,
-            secret='True',
-            passphrase='passphrase'
-    )
-    
-    with open('mykeyfile.asc', 'w') as f:
-        f.write(ascii_armored_public_keys)
-        f.write(ascii_armored_private_keys)
 
 def import_keys(): #local
+    
     with open('mykeyfile.asc', 'r') as f:
         key_data = f.read()
 
     import_result = gpg.import_keys(key_data)
-    for k in import_result.results:
-        print(k)
-
+    
     return import_result
 
 def import_keys_server():
-    
-    import_result = gpg.recv_keys('server-name', 'keyid')
+    pass
+    #import_result = gpg.recv_keys('server-name', 'keyid')
 
 
 def export_keys_server(gpg, keys): #Sending keys
+    pass
     #gpg.send_keys(SERVER, PRIVATE_KEY_ID)
 
-def help() 
-    parser = argparse.ArgumentParser(description="gerenciador de chaves gpg")
-    parser.add_argument("--name", action="store", dest="name_usr")
-    parser.add_argument("--email", action="store", dest="email_usr")
-    parser.add_argument("--passphrase", action="store", dest="passphrase")
+def delete_keys():
+    
+    public_keys = gpg.list_keys() 
+    private_keys = gpg.list_keys(secret=True)
+    
+    for key in private_keys:
+        gpg.delete_keys(key['fingerprint'], True, passphrase="disney")
+    
+    for key in public_keys:
+        gpg.delete_keys(key['fingerprint'])
+    
+def list_keys():
+    
+    public_keys = gpg.list_keys() 
+    private_keys = gpg.list_keys(secret=True)
+        
+    print("public keys: ", public_keys)
+    print()
+    print("private keys: ", private_keys)
 
+def export_keys(gpg, key, passphrase):
+    
+    ascii_armored_public_keys = gpg.export_keys(key.fingerprint)
+    ascii_armored_private_keys= gpg.export_keys(
+            keyids = key.fingerprint,
+            secret = 'True',
+            passphrase = passphrase
+    )
+
+def create_key(gpg):
+    
+    user = {
+        'name_real' : "Bisso",
+        'name_email' : "bisso@teste.net",
+        'expire_date' : '2019-09-10',
+        'key_type': 'RSA',
+        'key_length': 4096,
+        'key_usage': '',
+        #'subkey_type':'RSA',
+        #'subkey_length' : "encrypt,sign,auth",
+        'passphrase': "disney" }
+
+    user_input = gpg.gen_key_input(**user)
+    #print(user_input)
+    user_key = gpg.gen_key(user_input)
+    #print("user_key: ", user_key.fingerprint)
+    #print("user_key: ", user_key)
+    export_keys(gpg, user_key, user['passphrase'])
+    
+
+def help(): 
+    parser = argparse.ArgumentParser(description="gerenciador de chaves gpg")
+    
+    parser.add_argument("--criar-chave", action="store_true")
+    parser.add_argument("--name", action="store", dest="user_name")
+    parser.add_argument("--email", action="store", dest="use_email")
+    parser.add_argument("--passphrase", action="store", dest="user_passphrase")
+    
     args = parser.parse_args()
     
+    print (args)
+    
+    #parser = argparse.ArgumentParser()
+
+
     return args
 
 if __name__ == "__main__":
 
-    
-    """
-    if len(sys.argv) < 2 :
-        print("Uso: pyhton3 " +sys.argv[0]+ " <arquivo_mensagem>")
-        sys.exit(0) 
-
-    file_message = sys.argv[1]
-    """
     args = help()
-    gpg = gnupg.GPG(gnupghome=os.getcwd())
-    imput_data = gpg.gen_key_input(
-            name_email='my@email.com',
-            passphrase='passphrase',
-    )
-
-    key = gpg.gen_key(imput_data)
-    print(key)
     
-    """
-    print(status.ok)
-    print(status.status)
-    print(status.stderr)
-    """
+    #print(args)
+    #print(args.user_name) 
+    gpg = gnupg.GPG(gnupghome=os.getcwd())
+    
+    #create_key(gpg) 
+    #delete_keys()
+    #list_keys()
+
+
+  
